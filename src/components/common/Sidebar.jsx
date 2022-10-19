9c99ec5a619480e205784c2b09859cdf1de53e9b
webethics <info@webethicssolutions.com>
2022-10-19 13:08:49 +0530

    resolve the scrollbar issue in the sidebar.

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -60,7 +60,7 @@ const Sidebar = () => {
             <MenuIcon />
          </IconButton>
 
-         <Drawer anchor="left" open={isDrawerOpen} onClose={() => setIsDrawerOpen(false)} className={openProfile ? 'dropdown-open' : ''}>
+         <Drawer anchor="left" open={isDrawerOpen} onClose={() => setIsDrawerOpen(false)} className={openProfile ? 'dropdown-opens' : ''}>
             <Box px={2} width={{ xs: "76px", lg: "225px" }} role="presentation" display="flex" flexWrap="wrap" flexDirection="column" sx={{ flex: 1, boxSizing: "border-box" }}>
                {/* Sidebar Logo */}
                <Avatar alt="Remy Sharp" src={Logo} variant="rounded" sx={{ pt: 2, pb: 1, width: "100%", display: { xs: "none", lg: "flex" }, height: 'auto', position: 'sticky', top: 0, zIndex: 9, background: 'white' }} />
@@ -78,7 +78,7 @@ const Sidebar = () => {
                   <ListItem disablePadding>
                      <ListItemButton component="a" href="#dashboard" sx={{ borderRadius: "3px", justifyContent: { xs: "center", lg: "start" } }} selected={selectedIndex === 0} onClick={() => handleListItemClick(0)}>
                         <ListItemIcon sx={{ minWidth: { xs: "initial", lg: "30px" } }}>
-                           <Tooltip title="Dashboard" placement="right" arrow>
+                           <Tooltip title="" placement="right" arrow>
                               <DashboardCustomizeIcon fontSize="small" sx={{ color: selectedIndex === 0 ? 'primary.dark' : 'sidebar.iconText' }} />
                            </Tooltip>
                         </ListItemIcon>
@@ -88,7 +88,7 @@ const Sidebar = () => {
                   <ListItem disablePadding>
                      <ListItemButton component="a" href="#team" sx={{ borderRadius: "3px", justifyContent: { xs: "center", lg: "start" } }} selected={selectedIndex === 1} onClick={() => handleListItemClick(1)}>
                         <ListItemIcon sx={{ minWidth: { xs: "initial", lg: "30px" } }}>
-                           <Tooltip title="Team" placement="right" arrow>
+                           <Tooltip title="" placement="right" arrow>
                               <Diversity3Icon fontSize="small" sx={{ color: selectedIndex === 1 ? 'primary.dark' : 'sidebar.iconText' }} />
                            </Tooltip>
                         </ListItemIcon>
@@ -107,7 +107,7 @@ const Sidebar = () => {
                   <ListItem disablePadding>
                      <ListItemButton component="a" href="#projects" sx={{ borderRadius: "3px", justifyContent: { xs: "center", lg: "start" } }} selected={selectedIndex === 7} onClick={() => handleListItemClick(7)}>
                         <ListItemIcon sx={{ minWidth: { xs: "initial", lg: "30px" } }}>
-                           <Tooltip title="Projects" placement="right" arrow>
+                           <Tooltip title="" placement="right" arrow>
                               <FolderCopyIcon fontSize="small" sx={{ color: selectedIndex === 7 ? 'primary.dark' : 'sidebar.iconText' }} />
                            </Tooltip>
                         </ListItemIcon>
@@ -117,7 +117,7 @@ const Sidebar = () => {
                   <ListItem disablePadding>
                      <ListItemButton component="a" href="#category" sx={{ borderRadius: "3px", justifyContent: { xs: "center", lg: "start" } }} selected={selectedIndex === 8} onClick={() => handleListItemClick(8)}>
                         <ListItemIcon sx={{ minWidth: { xs: "initial", lg: "30px" } }}>
-                           <Tooltip title="Category" placement="right" arrow>
+                           <Tooltip title="" placement="right" arrow>
                               <AccountTreeIcon fontSize="small" sx={{ color: selectedIndex === 8 ? 'primary.dark' : 'sidebar.iconText' }} />
                            </Tooltip>
                         </ListItemIcon>
@@ -127,7 +127,7 @@ const Sidebar = () => {
                   <ListItem disablePadding>
                      <ListItemButton component="a" href="#tasks" sx={{ borderRadius: "3px", justifyContent: { xs: "center", lg: "start" } }} selected={selectedIndex === 9} onClick={() => handleListItemClick(9)}>
                         <ListItemIcon sx={{ minWidth: { xs: "initial", lg: "30px" } }}>
-                           <Tooltip title="Tasks" placement="right" arrow>
+                           <Tooltip title="" placement="right" arrow>
                               <AssignmentIcon fontSize="small" sx={{ color: selectedIndex === 9 ? 'primary.dark' : 'sidebar.iconText' }} />
                            </Tooltip>
                         </ListItemIcon>
@@ -137,7 +137,7 @@ const Sidebar = () => {
                   <ListItem disablePadding>
                      <ListItemButton component="a" href="#credentials" sx={{ borderRadius: "3px", justifyContent: { xs: "center", lg: "start" } }} selected={selectedIndex === 10} onClick={() => handleListItemClick(10)}>
                         <ListItemIcon sx={{ minWidth: { xs: "initial", lg: "30px" } }}>
-                           <Tooltip title="Credentials" placement="right" arrow>
+                           <Tooltip title="" placement="right" arrow>
                               <PasswordIcon fontSize="small" sx={{ color: selectedIndex === 10 ? 'primary.dark' : 'sidebar.iconText' }} />
                            </Tooltip>
                         </ListItemIcon>
@@ -156,7 +156,7 @@ const Sidebar = () => {
                   <ListItem disablePadding>
                      <ListItemButton component="a" href="#interviews" sx={{ borderRadius: "3px", justifyContent: { xs: "center", lg: "start" } }} selected={selectedIndex === 11} onClick={() => handleListItemClick(11)}>
                         <ListItemIcon sx={{ minWidth: { xs: "initial", lg: "30px" } }}>
-                           <Tooltip title="Interviews" placement="right" arrow>
+                           <Tooltip title="" placement="right" arrow>
                               <PendingActionsIcon fontSize="small" sx={{ color: selectedIndex === 11 ? 'primary.dark' : 'sidebar.iconText' }} />
                            </Tooltip>
                         </ListItemIcon>
@@ -166,7 +166,7 @@ const Sidebar = () => {
                   <ListItem disablePadding>
                      <ListItemButton component="a" href="#questions" sx={{ borderRadius: "3px", justifyContent: { xs: "center", lg: "start" } }} selected={selectedIndex === 12} onClick={() => handleListItemClick(12)}>
                         <ListItemIcon sx={{ minWidth: { xs: "initial", lg: "30px" } }}>
-                           <Tooltip title="Questions" placement="right" arrow>
+                           <Tooltip title="" placement="right" arrow>
                               <HelpCenterIcon fontSize="small" sx={{ color: selectedIndex === 12 ? 'primary.dark' : 'sidebar.iconText' }} />
                            </Tooltip>
                         </ListItemIcon>
@@ -176,7 +176,7 @@ const Sidebar = () => {
                   <ListItem disablePadding>
                      <ListItemButton component="a" href="#employees" sx={{ borderRadius: "3px", justifyContent: { xs: "center", lg: "start" } }} selected={selectedIndex === 13} onClick={() => handleListItemClick(13)}>
                         <ListItemIcon sx={{ minWidth: { xs: "initial", lg: "30px" } }}>
-                           <Tooltip title="Employees" placement="right" arrow>
+                           <Tooltip title="" placement="right" arrow>
                               <PeopleIcon fontSize="small" sx={{ color: selectedIndex === 13 ? 'primary.dark' : 'sidebar.iconText' }} />
                            </Tooltip>
                         </ListItemIcon>
@@ -196,7 +196,7 @@ const Sidebar = () => {
                   <ListItem disablePadding>
                      <ListItemButton component="a" href="#sales" sx={{ borderRadius: "3px", justifyContent: { xs: "center", lg: "start" } }} selected={selectedIndex === 14} onClick={() => handleListItemClick(14)}>
                         <ListItemIcon sx={{ minWidth: { xs: "initial", lg: "30px" } }}>
-                           <Tooltip title="Sales" placement="right" arrow>
+                           <Tooltip title="" placement="right" arrow>
                               <ShowChartIcon fontSize="small" sx={{ color: selectedIndex === 14 ? 'primary.dark' : 'sidebar.iconText' }} />
                            </Tooltip>
                         </ListItemIcon>
@@ -234,7 +234,7 @@ const Sidebar = () => {
                   <ListItem disablePadding sx={{ marginTop: 'auto', px: 2 }}>
                      <ListItemButton component="a" href="#settings" sx={{ borderRadius: "3px", justifyContent: 'center', px: 2 }} onClick={() => handleListItemClick(15)}>
                         <ListItemIcon sx={{ minWidth: { xs: "initial", lg: "30px" } }}>
-                           <Tooltip title="Settings" placement="right" arrow>
+                           <Tooltip title="" placement="right" arrow>
                               <SettingsIcon fontSize="small" sx={{ color: 'sidebar.iconText' }} />
                            </Tooltip>
                         </ListItemIcon>
@@ -244,7 +244,7 @@ const Sidebar = () => {
                   <ListItem disablePadding sx={{ marginTop: 'auto', px: 2 }}>
                      <ListItemButton component="a" href="#logout" sx={{ borderRadius: "3px", flexDirection: { xs: 'column', lg: 'row' } }} onClick={() => handleListItemClick(16)} className="">
                         <ListItemIcon sx={{ minWidth: { xs: "initial", lg: "30px" } }}>
-                           <Tooltip title="Logout" placement="right" arrow>
+                           <Tooltip title="" placement="right" arrow>
                               <ExitToAppIcon fontSize="small" sx={{ color: 'sidebar.iconText' }} />
                            </Tooltip>
                         </ListItemIcon>
